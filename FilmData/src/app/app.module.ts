e8382624f6647c4b34e8665d285a7d07b46f63eb
SrGarfia <garfia.bemig23@triana.salesianos.edu>
2024-11-13 12:06:55 +0100

    Empezando el componente de actores

--- FilmData/src/app/app.module.ts
+++ FilmData/src/app/app.module.ts
@@ -7,10 +7,12 @@ import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { MaterialModule } from './modules/material.module';
 import { PopularMoviesComponent } from './components/popular-movies/popular-movies.component';
+import { ActorsComponent } from './components/actors/actors.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ActorsComponent
   ],
   imports: [
     BrowserModule,
---
+++ FilmData/src/app/components/actors/actors.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-actors',
+  templateUrl: './actors.component.html',
+  styleUrl: './actors.component.css'
+})
+export class ActorsComponent {
+
+}
